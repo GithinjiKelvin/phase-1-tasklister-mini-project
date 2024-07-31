5c5f8c2642d774352c0a21cf6ed5ff87622eb717
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-07-31 03:36:22 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    createToDo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function createToDo(toDo){
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = `X`
+  li.textContent = `${toDo} `
+  li.appendChild(btn)
+  document.getElementById("tasks").appendChild(li)
+  
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
